7a1e5d0f84fb18e255bc852880dad82a0b5e1964
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-03-08 14:08:58 +0100

    bump sound

--- objects/player/player_movement.gd
+++ objects/player/player_movement.gd
@@ -1,33 +1,33 @@
-extends CharacterBody2D 
+extends CharacterBody2D
 class_name CharacterMovement
 
-@export var speed : float = 450 
-@export var player_radius : float = 63
-@export var collide_effect : PackedScene
-@export var camera : Camera2D
+@export var speed: float = 450
+@export var player_radius: float = 63
+@export var collide_effect: PackedScene
+@export var camera: Camera2D
+@export var bump_maker: AudioStreamPlayer
 
-@export var cam_zoom : Vector2
-@export var speed_cam_zoom : Vector2
-@export var zoom_speed : float = 0.4
+@export var cam_zoom: Vector2
+@export var speed_cam_zoom: Vector2
+@export var zoom_speed: float = 0.4
 
 signal died()
 
 signal start()
 signal ended()
 
-var direction : Vector2 = Vector2(0,0)
-var pressed : bool = false
+var direction: Vector2 = Vector2(0, 0)
+var pressed: bool = false
 
-var t : Tween
+var t: Tween
 
 func _ready():
 	Engine.time_scale = 1
 
 func _input(event):
-
 	if event.is_action_pressed('m1') && !pressed:
 		pressed = true
-		turn_on()	
+		turn_on()
 
 	if event.is_action_released("m1") && pressed:
 		redirect()
@@ -42,23 +42,21 @@ func die() -> void:
 	died.emit()
 
 func turn_on() -> void:
-
 	if t:
 		t.kill()
 
 	t = get_tree().create_tween()
-	t.tween_property(camera,"zoom",speed_cam_zoom,zoom_speed).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	t.tween_property(camera, "zoom", speed_cam_zoom, zoom_speed).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 
 	start.emit()
 	Engine.time_scale = 0.3
 
 func turn_off() -> void:
-
 	if t:
 		t.kill()
 
 	t = get_tree().create_tween()
-	t.tween_property(camera,"zoom",cam_zoom,zoom_speed).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	t.tween_property(camera, "zoom", cam_zoom, zoom_speed).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 
 
 	ended.emit()
@@ -67,59 +65,51 @@ func turn_off() -> void:
 func is_collide() -> void:
 	var space_state = get_world_2d().direct_space_state
 	
-	var ray_length = player_radius*1.1
+	var ray_length = player_radius * 1.1
 	var dir = direction.normalized()
 
 	var perp = Vector2(-dir.y, dir.x)
 
 	var query = PhysicsRayQueryParameters2D.create(
 		global_position,
-		global_position + dir * ray_length 
+		global_position + dir * ray_length
 	)
-	query.exclude = [self]
+	query.exclude = [ self ]
 
 	var query_left = PhysicsRayQueryParameters2D.create(
 		global_position + perp * player_radius,
 		global_position + perp * player_radius + dir * ray_length
 	)
-	query_left.exclude = [self]
+	query_left.exclude = [ self ]
 
 	var query_right = PhysicsRayQueryParameters2D.create(
 		global_position - perp * player_radius,
 		global_position - perp * player_radius + dir * ray_length
 	)
-	query_right.exclude = [self]
+	query_right.exclude = [ self ]
 
 	var result = space_state.intersect_ray(query)
 	var result_left = space_state.intersect_ray(query_left)
 	var result_right = space_state.intersect_ray(query_right)
 
 	if result:
-		direction = direction.bounce(result.normal)
-		
-		if result.collider is Enemy:
-			result.collider.die()
-
-		spawn_effect(result.position)
+		bounce(result.normal,result.collider,result.position)
 	elif result_left:
-		direction = direction.bounce(result_left.normal)
-
-		if result_left.collider is Enemy:
-			result_left.collider.die()
-
-		spawn_effect(result_left.position)
+		bounce(result_left.normal,result_left.collider,result_left.position)
 	elif result_right:
-		direction = direction.bounce(result_right.normal)
+		bounce(result_right.normal,result_right.collider,result_right.position)
 
-		if result_right.collider is Enemy:
-			result_right.collider.die()
+func bounce(normal: Vector2,collider: Node2D, pos : Vector2) -> void:
+	direction = direction.bounce(normal)
 
+	if collider is Enemy:
+		collider.die()
 
-		spawn_effect(result_right.position)
+	bump_maker.play()
+	spawn_effect(pos)
 
-
-func spawn_effect(pos : Vector2) -> void:
-	var ef : OneshotEffect =  collide_effect.instantiate()
+func spawn_effect(pos: Vector2) -> void:
+	var ef: OneshotEffect = collide_effect.instantiate()
 
 	ef.restart()
 	ef.global_position = pos
@@ -128,14 +118,12 @@ func spawn_effect(pos : Vector2) -> void:
 
 
 func _physics_process(_delta: float) -> void:
-
 	if direction == Vector2.ZERO:
 		return
 
 
-
 	is_collide()
-	velocity = direction*speed
+	velocity = direction * speed
 	move_and_slide()
 		
 #func _draw():
